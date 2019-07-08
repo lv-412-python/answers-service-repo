e3e3424415457624f89620f4a288f6ea0d967f9e
LSikyr <sikyrynskyi@gmail.com>
2019-07-08 11:32:18 +0300

    Added timestamp and primary key constraint

--- answers_service/models/answer.py
+++ answers_service/models/answer.py
@@ -1,14 +1,15 @@
 """ answers service model """
-from sqlalchemy import Column, Integer, String
+import datetime
+from sqlalchemy import Column, Integer, String, DateTime, PrimaryKeyConstraint
 from answers_service.db import DB
 
 
 class Answer(DB.Model):  # pylint: disable=too-few-public-methods
     """ models """
-    extend_existing = True
-    id = Column(Integer(), primary_key=True)
     reply = Column(String(200), nullable=False)
     user_id = Column(Integer(), nullable=False)
     form_id = Column(Integer(), nullable=False)
     field_id = Column(Integer(), nullable=False)
     group_id = Column(Integer(), nullable=False)
+    answer_date = Column(DateTime(), default=datetime.datetime.utcnow)
+    __table_args__ = (PrimaryKeyConstraint('user_id', 'form_id', 'field_id', name='answer_pk'), )
