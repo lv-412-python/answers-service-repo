f45d653ebe4cc0f511175e85b03227de556c6b62
LSikyr <sikyrynskyi@gmail.com>
2019-07-09 12:20:55 +0300

    merged get methods into one, added URL filter

--- answers_service/views/resources.py
+++ answers_service/views/resources.py
@@ -1,8 +1,6 @@
 # pylint: disable=cyclic-import
 """API routes"""
 from answers_service import API
-from .answers import UserAnswer, GroupAnswers, FormAnswers
+from .answers import UserAnswer
 
-API.add_resource(UserAnswer, '/answers/new')
-API.add_resource(GroupAnswers, '/answers/group/<form_id>/<group_id>')
-API.add_resource(FormAnswers, '/answers/form/<form_id>')
+API.add_resource(UserAnswer, '/answers/')
