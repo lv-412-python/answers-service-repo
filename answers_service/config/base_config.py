78fbffc30484b3d15e772782a4320f74d0b06ba3
LSikyr <sikyrynskyi@gmail.com>
2019-07-23 13:40:30 +0300

    changed postgres URL

--- answers_service/config/base_config.py
+++ answers_service/config/base_config.py
@@ -6,5 +6,5 @@ class Config:
     DEBUG = False
     TESTING = False
     SECRET_KEY = '3wffe3423@#Rr23krpo43o4t'
-    SQLALCHEMY_DATABASE_URI = 'postgres://@db:5432/4m_answers'
+    SQLALCHEMY_DATABASE_URI = 'postgres://postgres:mysecretpassword@db:5432/4m_answers'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
